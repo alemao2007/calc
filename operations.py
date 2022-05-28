35b7c830588d7fcf7aff054f579cfed95580222a
alemao2007 <106440311+alemao2007@users.noreply.github.com>
2022-05-28 13:06:59 -0300

    basic operations
    need to be implemented

---
+++ operations.py
@@ -0,0 +1,29 @@
+def add():
+  pass
+
+def sub():
+  pass
+
+def mul():
+  pass
+
+def div():
+  pass
+
+def rem():
+  pass
+
+def pow():
+  pass
+
+def dec_bin():
+  pass
+
+def bin_dec():
+  pass
+
+def hex_dec():
+  pass
+
+def dec_hex():
+  pass
